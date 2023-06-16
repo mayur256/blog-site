4296532120067b65b2b86dac3fdb445834148730
Mayur Upadhayay <mayur123a@gmail.com>
2023-06-17 00:46:52 +0530

    chore: Adding files for blog posts

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -38,6 +38,7 @@ export const Layout: React.FC<IProps> = ({
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
                 </ul>
             </nav>
             <main>
